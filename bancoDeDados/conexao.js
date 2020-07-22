49616fc1d896784df96df4bc1d8dee5c62fe1c0a
= <=>
2020-07-22 09:27:48 -0300

    Tem que ir agora

--- bancoDeDados/conexao.js
+++ bancoDeDados/conexao.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const conexao = new Sequelize('lojanode','gabriel','palmeiras12',{
+const conexao = new Sequelize('lojanode','gabriel123','palmeiras12',{
     host:'mysql669.umbler.com',
     dialect:'mysql',
     timezone:'-03:00'
